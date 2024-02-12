d3822be4dd95fe28cf6e9a93f99cbc61b64a5950
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-12 15:54:43 +0100

    displays Firebase user document data

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { Auth, user } from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import {
+  DocumentReference,
+  Firestore,
+  doc,
+  docData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  private userDoc: DocumentReference | undefined;
+  user$: Observable<any> | undefined;
 
-   constructor(public afAuth: Auth){}
-   user$ = user(this.afAuth);
+  constructor(
+      private afAuth: Auth, 
+      private afStore: Firestore
+   ) {}
 
+  ngOnInit(): void {
+    this.userDoc = doc(
+      this.afStore,
+      `users/${this.afAuth.currentUser?.uid ?? ''}`
+    );
+    this.user$ = docData(this.userDoc);
+  }
 }
