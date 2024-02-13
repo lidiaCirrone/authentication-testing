9ed2316dc8ab571a838f144dec83a0e918866180
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-13 02:02:01 +0100

    displays collection of users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  CollectionReference,
+  Firestore,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent {
+  private usersCollection: CollectionReference<any> | undefined;
+  users: Observable<any>;
 
+  constructor(private afStore: Firestore) {
+    this.usersCollection = collection(this.afStore, 'users');
+    this.users = collectionData(this.usersCollection);
+  }
 }
